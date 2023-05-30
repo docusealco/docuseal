e45bad97f551e27a258f0a24bb8e6831ea0010d4
Alex Turchyn <lexfox777@gmail.com>
2023-05-30 20:00:40 +0300

    use own theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,19 @@ module.exports = {
   daisyui: {
     themes: [
       {
-        cupcake: {
-          ...require('daisyui/src/colors/themes')['[data-theme=cupcake]'],
-          primary: '#E4E0E1'
+        docuseal: {
+          'color-scheme': 'light',
+          primary: '#e4e0e1',
+          secondary: '#ef9fbc',
+          accent: '#eeaf3a',
+          neutral: '#291334',
+          'base-100': '#faf7f5',
+          'base-200': '#efeae6',
+          'base-300': '#e7e2df',
+          'base-content': '#291334',
+          '--rounded-btn': '1.9rem',
+          '--tab-border': '2px',
+          '--tab-radius': '.5rem'
         }
       }
     ]
