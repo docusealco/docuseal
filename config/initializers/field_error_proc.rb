4905b13f251e55e1fbd2cfa5bb42d9d5b5befd14
Alex Turchyn <lexfox777@gmail.com>
2023-06-02 01:23:56 +0300

    update form error styles

--- config/initializers/field_error_proc.rb
+++ config/initializers/field_error_proc.rb
@@ -8,8 +8,12 @@ ActionView::Base.field_error_proc = proc do |html_tag, instance|
   field_name =
     instance.object.class.human_attribute_name(instance.instance_variable_get(:@method_name).to_s)
 
+  parsed_html_tag = Nokogiri::HTML::DocumentFragment.parse(html_tag)
+  parsed_html_tag.children.add_class 'input-error'
+  html_tag = parsed_html_tag.to_s.html_safe
+
   result = html_tag
-  result += ApplicationController.helpers.tag.div("#{field_name} #{errors}") if errors.present?
+  result += ApplicationController.helpers.tag.label(ApplicationController.render(partial: 'shared/field_error', locals: { message: "#{field_name} #{errors}" }), class: 'label') if errors.present?
 
   result
 end
