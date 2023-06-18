866bf7291d44d93b77cf616b50d917bd5a33b6a5
Alex Turchyn <lexfox777@gmail.com>
2023-06-18 16:26:51 +0000

    add the ability to clone a template

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -3,6 +3,8 @@
 class TemplatesController < ApplicationController
   layout false
 
+  before_action :load_base_template, only: %i[new create]
+
   def show
     @template = current_account.templates.preload(documents_attachments: { preview_images_attachments: :blob })
                                .find(params[:id])
@@ -10,13 +12,22 @@ class TemplatesController < ApplicationController
 
   def new
     @template = current_account.templates.new
+    @template.name = "#{@base_template.name} (Clone)" if @base_template
   end
 
   def create
-    @template = current_account.templates.new(template_params)
+    @template =
+      if @base_template
+        current_account.templates.new(**@base_template.slice(:fields, :schema, :submitters), **template_params)
+      else
+        current_account.templates.new(template_params)
+      end
+
     @template.author = current_user
 
     if @template.save
+      Templates::CloneAttachments.call(template: @template, original_template: @base_template) if @base_template
+
       redirect_to template_path(@template)
     else
       render turbo_stream: turbo_stream.replace(:modal, template: 'templates/new'), status: :unprocessable_entity
@@ -34,5 +45,13 @@ class TemplatesController < ApplicationController
 
   def template_params
     params.require(:template).permit(:name)
+  end
+
+  def load_base_template
+    return if params[:base_template_id].blank?
+
+    @base_template = current_account.templates
+                                    .preload(documents_attachments: :preview_images_attachments)
+                                    .find_by(id: params[:base_template_id])
   end
 end
---
+++ lib/templates/clone_attachments.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Templates
+  module CloneAttachments
+    module_function
+
+    def call(template:, original_template:)
+      original_template.documents.each do |document|
+        new_document = ActiveStorage::Attachment.create!(
+          uuid: document.uuid,
+          blob_id: document.blob_id,
+          name: 'documents',
+          record: template
+        )
+
+        document.preview_images_attachments.each do |preview_image|
+          ActiveStorage::Attachment.create!(
+            uuid: preview_image.uuid,
+            blob_id: preview_image.blob_id,
+            name: 'preview_images',
+            record: new_document
+          )
+        end
+      end
+    end
+  end
+end
