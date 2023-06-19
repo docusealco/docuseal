0b4a2819dd8c649043d38819f66517a4906708b1
Alex Turchyn <lexfox777@gmail.com>
2023-06-19 21:44:41 +0000

    add landing page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class DashboardController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[index]
+
   def index
+    return render 'pages/landing' unless signed_in?
+
     @templates = current_account.templates.active
   end
 end
