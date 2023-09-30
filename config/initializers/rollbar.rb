7f97bfb3bd6466caffc70e6ea87fb58d22c72c75
DocuSeal <dev@docuseal.co>
2023-09-30 11:33:51 +0300

    ensure rollbar required with sidekiq

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'rollbar' if ENV.key?('ROLLBAR_ACCESS_TOKEN')
+
 if defined?(Rollbar)
   Rollbar.configure do |config|
     config.access_token = ENV.fetch('ROLLBAR_ACCESS_TOKEN', nil)
