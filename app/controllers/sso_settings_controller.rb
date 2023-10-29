5ed08fa9ab7fe16ed93527fbfa6dd513e4b14faf
DocuSeal <dev@docuseal.co>
2023-10-29 13:36:06 +0200

    add sso settings placeholder

---
+++ app/controllers/sso_settings_controller.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class SsoSettingsController < ApplicationController
+  before_action :load_encrypted_config
+  authorize_resource :encrypted_config, only: :index
+  authorize_resource :encrypted_config, parent: false, except: :index
+
+  def index; end
+
+  private
+
+  def load_encrypted_config
+    @encrypted_config =
+      EncryptedConfig.find_or_initialize_by(account: current_account, key: 'saml_configs')
+  end
+end
