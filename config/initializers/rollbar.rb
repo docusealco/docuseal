151a5b85f3cc414d7c739a7ad89712e358a94431
Pete Matsyburka <pete.matsy@gmail.com>
2024-02-12 16:54:48 +0200

    configure rollbar

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -6,7 +6,21 @@ if defined?(Rollbar)
   Rollbar.configure do |config|
     config.access_token = ENV.fetch('ROLLBAR_ACCESS_TOKEN', nil)
 
+    config.transform << proc do |options|
+      data = options[:payload]['data']
+
+      if data[:request]
+        data[:request][:cookies] = {}
+        data[:request][:session] = {}
+        data[:request][:url] = data[:request][:url].to_s.sub(%r{(/[sde]/)\w{8}}, '\1********')
+      end
+    end
+
     config.enabled = true
+    config.collect_user_ip = false
+    config.anonymize_user_ip = true
+    config.scrub_headers += %w[X-Auth-Token Cookie X-Csrf-Token Referer]
+    config.scrub_fields += %i[slug uuid attachment_uuid]
 
     config.exception_level_filters['ActionController::RoutingError'] = 'ignore'
 
