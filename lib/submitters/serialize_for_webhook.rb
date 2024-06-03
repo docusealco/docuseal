db302858c421193dde625a9678220fbd3c857b65
Pete Matsyburka <pete.matsy@gmail.com>
2024-06-03 18:13:13 +0300

    add template folder name to webhook

--- lib/submitters/serialize_for_webhook.rb
+++ lib/submitters/serialize_for_webhook.rb
@@ -30,8 +30,8 @@ module Submitters
                       'audit_log_url' => submitter.submission.audit_log_url,
                       'submission_url' => r.submissions_preview_url(submitter.submission.slug,
                                                                     **Docuseal.default_url_options),
-                      'template' => submitter.template.as_json(only: %i[id name external_id created_at
-                                                                        updated_at]),
+                      'template' => submitter.template.as_json(only: %i[id name external_id created_at updated_at],
+                                                               methods: %i[folder_name]),
                       'submission' => {
                         **submitter.submission.slice(:id, :audit_log_url, :created_at),
                         status: submitter.submission.submitters.all?(&:completed_at?) ? 'completed' : 'pending',
