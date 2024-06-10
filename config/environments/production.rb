14418b93b1732bd974f30f87f3085730f11d6713
Pete Matsyburka <pete.matsy@gmail.com>
2024-06-10 17:17:45 +0300

    full error log in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,8 @@
 require 'active_support/core_ext/integer/time'
 require 'active_support/core_ext/string'
 
+Rails.backtrace_cleaner.remove_silencers!
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
