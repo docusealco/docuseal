961e0a1c891c59b3cac8fc0950081873e2bf2c0e
Pete Matsyburka <pete.matsy@gmail.com>
2024-09-21 13:05:31 +0300

    fix i18n

--- lib/email_messages.rb
+++ lib/email_messages.rb
@@ -4,7 +4,7 @@ module EmailMessages
   module_function
 
   def find_or_create_for_account_user(account, user, subject, body)
-    subject = SubmitterMailer::DEFAULT_INVITATION_SUBJECT if subject.blank?
+    subject = I18n.t(:you_are_invited_to_sign_a_document) if subject.blank?
 
     sha1 = Digest::SHA1.hexdigest({ subject:, body: }.to_json)
 
