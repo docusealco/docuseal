8773b30b1041c49f55f1176770fc4b5d5e3d741c
Pete Matsyburka <pete@docuseal.com>
2024-10-27 14:25:16 +0200

    adjust auto migration

--- config/initializers/migrate.rb
+++ config/initializers/migrate.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Rails.configuration.to_prepare do
-  ActiveRecord::Tasks::DatabaseTasks.migrate if ENV['RAILS_ENV'] == 'production'
+  ActiveRecord::Tasks::DatabaseTasks.migrate if ENV['RAILS_ENV'] == 'production' && ENV['RUN_MIGRATIONS'] != 'false'
 end
