22d48334964c51e8b9d30de11aa54f7c03cdb684
Pete Matsyburka <pete@docuseal.com>
2024-11-01 21:23:55 +0200

    allow to unarchive submissions

--- config/routes.rb
+++ config/routes.rb
@@ -69,7 +69,9 @@ Rails.application.routes.draw do
   resource :user_initials, only: %i[edit update destroy]
   resources :submissions_archived, only: %i[index], path: 'submissions/archived'
   resources :submissions, only: %i[index], controller: 'submissions_dashboard'
-  resources :submissions, only: %i[show destroy]
+  resources :submissions, only: %i[show destroy] do
+    resources :unarchive, only: %i[create], controller: 'submissions_unarchive'
+  end
   resources :submitters, only: %i[edit update]
   resources :console_redirect, only: %i[index]
   resources :upgrade, only: %i[index], controller: 'console_redirect'
