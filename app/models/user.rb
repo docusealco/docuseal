9ce822bce8359dc3e35871cdd77b41844b1b9fb9
Pete Matsyburka <pete@docuseal.com>
2024-12-22 11:33:15 +0200

    add user tokens

--- app/models/user.rb
+++ app/models/user.rb
@@ -57,6 +57,7 @@ class User < ApplicationRecord
 
   belongs_to :account
   has_one :access_token, dependent: :destroy
+  has_many :access_tokens, dependent: :destroy
   has_many :templates, dependent: :destroy, foreign_key: :author_id, inverse_of: :author
   has_many :template_folders, dependent: :destroy, foreign_key: :author_id, inverse_of: :author
   has_many :user_configs, dependent: :destroy
