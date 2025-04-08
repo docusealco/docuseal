f090e79002e88e419edd6f2ef5cb642e0c87dda9
Pete Matsyburka <pete@docuseal.com>
2025-04-08 19:21:25 +0300

    fix required

--- app/controllers/submit_form_controller.rb
+++ app/controllers/submit_form_controller.rb
@@ -71,7 +71,7 @@ class SubmitFormController < ApplicationController
 
     head :ok
   rescue Submitters::SubmitValues::RequiredFieldError => e
-    Rollbar.warning("Required field #{submitter.id}: #{e.message}") if defined?(Rollbar)
+    Rollbar.warning("Required field #{@submitter.id}: #{e.message}") if defined?(Rollbar)
 
     render json: { field_uuid: e.message }, status: :unprocessable_entity
   rescue Submitters::SubmitValues::ValidationError => e
