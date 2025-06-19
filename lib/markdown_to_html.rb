8f3a203994362a32eff68b04f44ba49826160652
Pete Matsyburka <pete@docuseal.com>
2025-06-19 12:42:44 +0300

    markdown link

--- lib/markdown_to_html.rb
+++ lib/markdown_to_html.rb
@@ -2,11 +2,12 @@
 
 module MarkdownToHtml
   LINK_REGEXP = %r{\[([^\]]+)\]\((https?://[^)]+)\)}
-  LINK_REPLACE = '<a href="\2">\1</a>'
 
   module_function
 
   def call(text)
-    text.gsub(LINK_REGEXP, LINK_REPLACE)
+    text.gsub(LINK_REGEXP) do
+      ApplicationController.helpers.link_to(Regexp.last_match(1), Regexp.last_match(2))
+    end
   end
 end
