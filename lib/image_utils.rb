ee4acd5a8274494b4908fe1ec96bd84ca8f78d1f
Pete Matsyburka <pete@docuseal.com>
2025-06-25 22:31:19 +0300

    validate signature image

---
+++ lib/image_utils.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module ImageUtils
+  module_function
+
+  def blank?(image)
+    min = (0...image.bands).map { |i| image.stats.getpoint(0, i)[0] }
+    max = (0...image.bands).map { |i| image.stats.getpoint(1, i)[0] }
+
+    return true if min.all?(255) && max.all?(255)
+    return true if min.all?(0) && max.all?(0)
+    return true if min.length >= 4 && min[-1].zero? && max[-1].zero?
+
+    false
+  end
+end
