91df5b24077c7dbd1ba1e57b912db5b77658af07
Pete Matsyburka <pete@docuseal.com>
2025-07-02 13:16:47 +0300

    optimize image blank

--- lib/image_utils.rb
+++ lib/image_utils.rb
@@ -4,8 +4,10 @@ module ImageUtils
   module_function
 
   def blank?(image)
-    min = (0...image.bands).map { |i| image.stats.getpoint(0, i)[0] }
-    max = (0...image.bands).map { |i| image.stats.getpoint(1, i)[0] }
+    stats = image.stats
+
+    min = (0...image.bands).map { |i| stats.getpoint(0, i)[0] }
+    max = (0...image.bands).map { |i| stats.getpoint(1, i)[0] }
 
     return true if min.all?(255) && max.all?(255)
     return true if min.all?(0) && max.all?(0)
