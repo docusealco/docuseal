47a6ed69805397692d3d69d1d97bdd3f9e25100f
Mikhael Rakauskas <mrakauskas@careerplug.com>
2025-08-21 13:37:34 -0400

    Unique fix for benchmarking

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
+require 'benchmark'
+
 class ApplicationJob < ActiveJob::Base
   include ::NewRelic::Agent::Instrumentation::ControllerInstrumentation
+  unique :while_executing, on_conflict: :log
 
   retry_on StandardError, wait: 6.seconds, attempts: 5 unless Docuseal.multitenant?
-  unique :while_executing, on_conflict: :log
 
   def perform(*args)
     receiver_str, _, message = args.shift.rpartition('.')
