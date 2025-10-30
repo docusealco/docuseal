4eb3ce22b16486cdda6ebe34f7668a3c3b15c79e
Bernardo Anderson <bern.anderson@proton.me>
2025-10-30 09:59:10 -0500

    CP-11289 - Migration updates

--- app/models/partnership.rb
+++ app/models/partnership.rb
@@ -8,7 +8,7 @@
 #  name                    :string           not null
 #  created_at              :datetime         not null
 #  updated_at              :datetime         not null
-#  external_partnership_id :integer          not null
+#  external_partnership_id :string           not null
 #
 # Indexes
 #
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_24_174100) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_30_175543) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "btree_gin"
   enable_extension "pg_catalog.plpgsql"
 
   create_table "access_tokens", force: :cascade do |t|
@@ -105,7 +104,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_24_174100) do
     t.string "sha256", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "storage_location", default: "secured"
     t.index ["sha256"], name: "index_completed_documents_on_sha256"
+    t.index ["storage_location"], name: "index_completed_documents_on_storage_location"
     t.index ["submitter_id"], name: "index_completed_documents_on_submitter_id"
   end
 
@@ -162,7 +163,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_24_174100) do
     t.string "event_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["submitter_id", "event_name"], name: "index_document_generation_events_on_submitter_id_and_event_name", unique: true, where: "((event_name)::text = ANY ((ARRAY['start'::character varying, 'complete'::character varying])::text[]))"
+    t.index ["submitter_id", "event_name"], name: "index_document_generation_events_on_submitter_id_and_event_name", unique: true, where: "((event_name)::text = ANY (ARRAY[('start'::character varying)::text, ('complete'::character varying)::text]))"
     t.index ["submitter_id"], name: "index_document_generation_events_on_submitter_id"
   end
 
@@ -179,7 +180,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_24_174100) do
     t.datetime "created_at", null: false
     t.index ["account_id", "event_datetime"], name: "index_email_events_on_account_id_and_event_datetime"
     t.index ["email"], name: "index_email_events_on_email"
-    t.index ["email"], name: "index_email_events_on_email_event_types", where: "((event_type)::text = ANY ((ARRAY['bounce'::character varying, 'soft_bounce'::character varying, 'complaint'::character varying, 'soft_complaint'::character varying])::text[]))"
+    t.index ["email"], name: "index_email_events_on_email_event_types", where: "((event_type)::text = ANY (ARRAY[('bounce'::character varying)::text, ('soft_bounce'::character varying)::text, ('complaint'::character varying)::text, ('soft_complaint'::character varying)::text]))"
     t.index ["emailable_type", "emailable_id"], name: "index_email_events_on_emailable"
     t.index ["message_id"], name: "index_email_events_on_message_id"
   end
@@ -274,24 +275,11 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_24_174100) do
   end
 
   create_table "partnerships", force: :cascade do |t|
-    t.integer "external_partnership_id", null: false
+    t.string "external_partnership_id", null: false
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["external_partnership_id"], name: "index_partnerships_on_external_partnership_id", unique: true
-  end
-
-  create_table "search_entries", force: :cascade do |t|
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "account_id", null: false
-    t.tsvector "tsvector", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["account_id", "tsvector"], name: "index_search_entries_on_account_id_tsvector_submission", where: "((record_type)::text = 'Submission'::text)", using: :gin
-    t.index ["account_id", "tsvector"], name: "index_search_entries_on_account_id_tsvector_submitter", where: "((record_type)::text = 'Submitter'::text)", using: :gin
-    t.index ["account_id", "tsvector"], name: "index_search_entries_on_account_id_tsvector_template", where: "((record_type)::text = 'Template'::text)", using: :gin
-    t.index ["record_id", "record_type"], name: "index_search_entries_on_record_id_and_record_type", unique: true
   end
 
   create_table "submission_events", force: :cascade do |t|
