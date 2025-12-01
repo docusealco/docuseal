5934bdec555fba6c8b5660f97d682d1d6fc2b188
Alex Turchyn <alex@docuseal.com>
2025-12-02 01:16:42 +0200

    use set_reset_password_token

--- spec/mailers/previews/devise_mailer_preview.rb
+++ spec/mailers/previews/devise_mailer_preview.rb
@@ -3,7 +3,7 @@
 class DeviseMailerPreview < ActionMailer::Preview
   def reset_password_instructions
     user = User.first
-    user.send_reset_password_instructions
-    Devise::Mailer.reset_password_instructions(user, user.reset_password_token)
+    token = user.send(:set_reset_password_token)
+    Devise::Mailer.reset_password_instructions(user, token)
   end
 end
