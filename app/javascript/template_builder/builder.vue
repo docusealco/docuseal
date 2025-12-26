0b3b95ff3840ab463f6c62941644dfa2154591ae
NEMYSESx <shakirraza4227@gmail.com>
2025-12-26 20:02:20 +0530

    feat: carry forward font and alignment attributes for new fields

--- app/javascript/template_builder/builder.vue
+++ app/javascript/template_builder/builder.vue
@@ -1210,6 +1210,21 @@ export default {
         }
       }
 
+      const lastField = this.selectedField || this.template.fields[this.template.fields.length - 1]
+
+      if (this.template.preferences?.carry_forward_attributes && lastField?.preferences) {
+        field.preferences ||= {}
+        field.preferences = { ...field.preferences }
+
+        const attrsToCopy = ['font', 'font_size', 'font_type', 'align', 'valign', 'color']
+
+        attrsToCopy.forEach((attr) => {
+          if (lastField.preferences[attr]) {
+            field.preferences[attr] = lastField.preferences[attr]
+          }
+        })
+      }
+
       if (field.type === 'strikethrough') {
         field.readonly = true
         field.default_value = true
@@ -1254,6 +1269,21 @@ export default {
         }
 
         this.drawField = field
+
+        const lastField = this.selectedField || this.template.fields[this.template.fields.length - 1]
+
+        if (this.template.preferences?.carry_forward_attributes && lastField?.preferences) {
+          field.preferences ||= {}
+          field.preferences = { ...field.preferences }
+
+          const attrsToCopy = ['font', 'font_size', 'font_type', 'align', 'valign', 'color']
+
+          attrsToCopy.forEach((attr) => {
+            if (lastField.preferences[attr]) {
+              field.preferences[attr] = lastField.preferences[attr]
+            }
+          })
+        }
       }
 
       this.drawOption = null
