6ca75c53ef667d06358dd358a305d58027798e55
Pete Matsyburka <pete@docuseal.com>
2026-01-05 12:46:43 +0200

    fix condition

--- app/javascript/template_builder/field_type.vue
+++ app/javascript/template_builder/field_type.vue
@@ -30,7 +30,7 @@
         v-for="(icon, type) in fieldIconsSorted"
         :key="type"
       >
-        <li v-if="fieldTypes.includes(type) || ((withPhone || type != 'phone') && (withPayment || type != 'payment') && (withVerification || type != 'verification') || (withKba || type != 'kba'))">
+        <li v-if="fieldTypes.includes(type) || ((withPhone || type != 'phone') && (withPayment || type != 'payment') && (withVerification || type != 'verification') && (withKba || type != 'kba'))">
           <a
             href="#"
             class="text-sm py-1 px-2"
