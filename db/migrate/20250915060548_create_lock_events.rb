e0ec443e10e2df284d0eacd11cc92d73231e5d07
Pete Matsyburka <pete@docuseal.com>
2026-01-05 12:34:50 +0200

    fix mysql migration

--- db/migrate/20250915060548_create_lock_events.rb
+++ db/migrate/20250915060548_create_lock_events.rb
@@ -6,7 +6,7 @@ class CreateLockEvents < ActiveRecord::Migration[8.0]
       t.string :key, index: true, null: false
       t.string :event_name, null: false
 
-      t.index %i[event_name key], unique: true, where: "event_name IN ('start', 'complete')"
+      t.index %i[event_name key], unique: adapter_name != 'Mysql2', where: "event_name IN ('start', 'complete')"
 
       t.timestamps
     end
