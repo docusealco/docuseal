5f76290010082e5aa81c4fd105990529b5117f80
disruptiverecords <ivo@disruptiverecords.nl>
2026-02-03 16:21:33 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,15 +6,15 @@ module.exports = {
     themes: [
       {
         docuseal: {
-          'color-scheme': 'light',
+          'color-scheme': 'dark',
           primary: '#e4e0e1',
           secondary: '#ef9fbc',
           accent: '#eeaf3a',
           neutral: '#291334',
-          'base-100': '#faf7f5',
-          'base-200': '#efeae6',
-          'base-300': '#e7e2df',
-          'base-content': '#291334',
+          'base-100': '#1a1a1a',
+          'base-200': '#eeaf3a',
+          'base-300': '#ededde',
+          'base-content': '#ededde',
           '--rounded-btn': '1.9rem',
           '--tab-border': '2px',
           '--tab-radius': '.5rem'
