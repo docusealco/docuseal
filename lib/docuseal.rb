79116c27a9fb5a816fad20b0df13061a4449f533
Leon Liang <hello@leonliang.lu>
2026-04-25 19:37:32 +0200

    change name to CSEESeal

--- lib/docuseal.rb
+++ lib/docuseal.rb
@@ -6,7 +6,7 @@ module Docuseal
   PRODUCT_EMAIL_URL = ENV.fetch('PRODUCT_EMAIL_URL', PRODUCT_URL)
   NEWSLETTER_URL = "#{PRODUCT_URL}/newsletters".freeze
   ENQUIRIES_URL = "#{PRODUCT_URL}/enquiries".freeze
-  PRODUCT_NAME = 'DocuSeal'
+  PRODUCT_NAME = 'CSEESeal'
   DEFAULT_APP_URL = ENV.fetch('APP_URL', 'http://localhost:3000')
   GITHUB_URL = 'https://github.com/docusealco/docuseal'
   DISCORD_URL = 'https://discord.gg/qygYCDGck9'
