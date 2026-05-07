df3697a64f5a2605fef8f2d6dee2b0154a56b874
Sebastian Noe <sebastian.schneider@boxine.de>
2026-05-07 16:34:25 +0200

    feat: add company logo upload and white-label display
    - Add has_one_attached :logo to Account model
    - Add AccountLogoController for upload/delete in Settings
    - Display custom logo in signing forms (start_form, submit_form)
    - Display custom logo in submission preview
    - Add logo upload form with drag-and-drop to personalization settings

---
+++ app/controllers/account_logo_controller.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class AccountLogoController < ApplicationController
+  before_action :authorize_account_config
+
+  def create
+    file = params[:file]
+
+    if file.blank?
+      return redirect_to settings_personalization_path, alert: I18n.t('unable_to_save')
+    end
+
+    current_account.logo.attach(file)
+
+    redirect_to settings_personalization_path, notice: I18n.t('settings_have_been_saved')
+  end
+
+  def destroy
+    current_account.logo.purge
+
+    redirect_to settings_personalization_path, notice: I18n.t('settings_have_been_saved')
+  end
+
+  private
+
+  def authorize_account_config
+    authorize!(:create, AccountConfig)
+  end
+end
