f70c6ebabf140c0a00e78b0bb8f15a73ab4d53e8
Sebastian Noe <github@snoe.me>
2026-05-08 20:38:01 +0200

    Customize navigation for fork: remove upstream references (#2)
    - Remove Discord, AI assistant, and support email from settings sidebar
    - Remove Console and Plans menu items from sidebar and navbar dropdown
    - Grey out SSO menu item (pro feature, not yet implemented)
    - Rename 'Upgrade' button to 'Go Full Pro' linking to docuseal.com
    - Point GitHub URL to snoe/docuseal-like-a-pro fork
    - Replace hardcoded GitHub URLs with Docuseal::GITHUB_URL constant
    
    Co-authored-by: Sebastian Noe <sebastian.schneider@boxine.de>

--- lib/docuseal.rb
+++ lib/docuseal.rb
@@ -7,7 +7,7 @@ module Docuseal
   ENQUIRIES_URL = "#{PRODUCT_URL}/enquiries".freeze
   PRODUCT_NAME = 'DocuSeal'
   DEFAULT_APP_URL = ENV.fetch('APP_URL', 'http://localhost:3000')
-  GITHUB_URL = 'https://github.com/docusealco/docuseal'
+  GITHUB_URL = 'https://github.com/snoe/docuseal-like-a-pro'
   DISCORD_URL = 'https://discord.gg/qygYCDGck9'
   TWITTER_URL = 'https://twitter.com/docusealco'
   TWITTER_HANDLE = '@docusealco'
