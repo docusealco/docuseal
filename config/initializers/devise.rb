232380d378a5fd2bf8a2410b5c932b3432be8418
Omar <omar25shaarawi@gmail.com>
2026-05-17 13:37:30 -0500

    Fix: require lib/docuseal in devise initializer (autoload not ready)

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -334,6 +334,7 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
 
+  require Rails.root.join('lib', 'docuseal').to_s
   if Docuseal.clerk_oidc_enabled?
     config.omniauth_path_prefix = '/users/auth'
     config.omniauth :openid_connect, {
