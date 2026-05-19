f153c8908527cc668dc387f00dd8037cb5844829
SuBroXz11 <subratkrp2@gmail.com>
2026-05-19 12:15:42 +0545

    feat: update primary and neutral colors to arcab blue #000fff

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,23 @@
 module.exports = {
-  plugins: [
-    require('daisyui')
-  ],
+  plugins: [require("daisyui")],
   daisyui: {
     themes: [
       {
         docuseal: {
-          'color-scheme': 'light',
-          primary: '#e4e0e1',
-          secondary: '#ef9fbc',
-          accent: '#eeaf3a',
-          neutral: '#291334',
-          'base-100': '#faf7f5',
-          'base-200': '#efeae6',
-          'base-300': '#e7e2df',
-          'base-content': '#291334',
-          '--rounded-btn': '1.9rem',
-          '--tab-border': '2px',
-          '--tab-radius': '.5rem'
-        }
-      }
-    ]
-  }
-}
+          "color-scheme": "light",
+          primary: "#000fff",
+          secondary: "#ef9fbc",
+          accent: "#eeaf3a",
+          neutral: "#000fff",
+          "base-100": "#faf7f5",
+          "base-200": "#efeae6",
+          "base-300": "#e7e2df",
+          "base-content": "#291334",
+          "--rounded-btn": "1.9rem",
+          "--tab-border": "2px",
+          "--tab-radius": ".5rem",
+        },
+      },
+    ],
+  },
+};
