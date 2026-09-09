a3828ec390df992b979807031ca3ce50a35dfadc
Pete Matsyburka <pete@docuseal.com>
2026-09-09 13:35:20 +0300

    xlsx write native boolean

--- lib/submissions/generate_export_files.rb
+++ lib/submissions/generate_export_files.rb
@@ -147,8 +147,6 @@ module Submissions
 
               ActiveStorage::Blob.proxy_url(attachment.blob, expires_at:) if attachment
             end
-          elsif submitter_value == true || submitter_value == false
-            submitter_value.to_s
           else
             submitter_value
           end
