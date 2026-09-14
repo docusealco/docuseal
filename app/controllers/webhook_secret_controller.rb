85fbae18137647e517cc0637b0816ace4c3a53b8
Pete Matsyburka <pete@docuseal.com>
2026-09-14 14:48:00 +0300

    validate webhook header

--- app/controllers/webhook_secret_controller.rb
+++ app/controllers/webhook_secret_controller.rb
@@ -3,11 +3,19 @@
 class WebhookSecretController < ApplicationController
   load_and_authorize_resource :webhook_url, parent: false
 
+  HEADER_NAME_REGEXP = /\A[\w-]+\z/
+
   def show; end
 
   def update
+    key = webhook_secret_params[:key]
+
+    if key.present? && !HEADER_NAME_REGEXP.match?(key)
+      return redirect_back(fallback_location: settings_webhook_path(@webhook_url), alert: I18n.t('unable_to_save'))
+    end
+
     @webhook_url.update!(secret: {
-      webhook_secret_params[:key] => webhook_secret_params[:value]
+      key => webhook_secret_params[:value]
     }.compact_blank)
 
     redirect_back(fallback_location: settings_webhook_path(@webhook_url),
